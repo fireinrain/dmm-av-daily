4c8dff8f749ce0c1bf353caeca1e91b14c24f50d
lzyme.dev@gmail.com <lzyme.dev@gmail.com>
2024-04-25 12:22:00 +0800

    fix old tg message push

--- tgbot.py
+++ tgbot.py
@@ -90,11 +90,11 @@ async def push_telegram_channel(run_date: str):
             dvd_id = utils.convert_cid2code(item.film_code)
             # tg markdownv2 不支持字符串中有- 所以要转义,tg caption 不支持-，
             # 所以设置为去掉
-            dvd_id = dvd_id.replace("-", "")
+            dvd_id2 = dvd_id.replace("-", "")
             caption = (f'番号: `{dvd_id}`, 演员: `{stars}`\n'
                        f'标题: `{film_title}`\n'
                        f'{translated_text}\n'
-                       f"\#D{formatted_date} \#{dvd_id} {' '.join(split_star)}")
+                       f"\#D{formatted_date} \#{dvd_id2} {' '.join(split_star)}")
 
             suffix = '#query#jump'
             # URLs for the buttons
@@ -188,11 +188,11 @@ async def patch_tg_channel_push():
             dvd_id = utils.convert_cid2code(film_detail.film_code)
             # tg markdownv2 不支持字符串中有- 所以要转义,tg caption 不支持-，
             # 所以设置为去掉
-            dvd_id = dvd_id.replace("-", "")
+            dvd_id2 = dvd_id.replace("-", "")
             caption = (f"番号: `{dvd_id}`, 演员: `{stars}`\n"
                        f"标题: `{film_title}`\n"
                        f"{translated_text}\n"
-                       f"\#D{formatted_date} \#{dvd_id} {' '.join(split_star)}")
+                       f"\#D{formatted_date} \#{dvd_id2} {' '.join(split_star)}")
 
             suffix = '#query#jump'
             # URLs for the buttons
